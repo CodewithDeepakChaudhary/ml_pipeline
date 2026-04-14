c24d44bebf4f6b8fbfcde1260b32ea46a96df705
Deepak Chaudhary <143191812+CodewithDeepakChaudhary@users.noreply.github.com>
2026-04-14 22:48:40 +0530

    Fix formatting issue in pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,8 @@ import streamlit as st
 defaults = {
     "data": None,
     "target": None,
-    "problem": None,import streamlit as st
+    "problem": None
+}
 import pandas as pd
 import numpy as np
 import plotly.express as px
